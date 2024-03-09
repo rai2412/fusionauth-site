ff352c936b1ddd2dc0a31ae79f9b64fc4cb4f2bb
Dan Moore <dan@fusionauth.io>
2024-03-09 10:29:29 -0700

    do bounding box clip every time

--- src/screenshots/tests/core-concepts/application.spec.js
+++ src/screenshots/tests/core-concepts/application.spec.js
@@ -3,8 +3,16 @@ import { test, expect } from '@playwright/test';
 const baseAdminURL = 'https://local.fusionauth.io/admin/';
 const baseImagePath = '../../astro/public/img/docs/';
 const browserWidth = 1280;
+const defaultBrowserHeight = 1200;
 const outlineCSS = '{ padding: 10px; border: 3px red solid; } ';
 
+function calculateClip(boxToGetBottomOf) {
+  const bottomY = (boxToGetBottomOf?.y + boxToGetBottomOf?.height )|| defaultBrowserHeight;
+  return {height: bottomY, width: browserWidth, x: 0, y: 0 }
+}
+
+   // const box = await page.locator('main.page-body').boundingBox();
+    // clip: calculateClip(box)
 
 test('core-concepts-applications-device-grant', async ({ page }) => {
 
@@ -21,6 +29,7 @@ test('core-concepts-applications-device-grant', async ({ page }) => {
     var deviceURL = await page.locator('#application_oauthConfiguration_deviceVerificationURL-form-row');
     await expect(deviceURL).toBeVisible();
 
+    // TODO change this to dynamically calculate height, not hardcode it
     await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-oauth-device-url.png', clip: {x: 0,y: 400, width: browserWidth, height:200} });
 });
 
@@ -30,7 +39,9 @@ test('core-concepts-applications-list', async ({ page }) => {
     await page.goto(baseAdminURL+'/application', {
       waitUntil: "networkidle"
     });
-    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/applications.png', clip: {x: 0,y: 0, width: browserWidth, height:600} });
+    const box = await page.locator('main.page-body').boundingBox();
+    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/applications.png', 
+      clip: calculateClip(box) });
   
 });
 
@@ -40,7 +51,8 @@ test('core-concepts-applications-edit-cleanspeak', async ({ page }) => {
     await page.goto(baseAdminURL+'/application/edit?applicationId=85a03867-dccf-4882-adde-1a79aeec50df&tenantId=30663132-6464-6665-3032-326466613934#cleanspeak-configuration', {
       waitUntil: "networkidle"
     });
-    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-cleanspeak.png', fullPage: true });
+    const box = await page.locator('main.page-body').boundingBox();
+    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-cleanspeak.png', clip: calculateClip(box) });
   
 });
 
@@ -50,7 +62,8 @@ test('core-concepts-applications-edit-jwt', async ({ page }) => {
     await page.goto(baseAdminURL+'/application/edit?applicationId=85a03867-dccf-4882-adde-1a79aeec50df&tenantId=30663132-6464-6665-3032-326466613934#jwt-settings', {
       waitUntil: "networkidle"
     });
-    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-jwt-disabled.png'});
+    const box = await page.locator('main.page-body').boundingBox();
+    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-jwt-disabled.png', clip: calculateClip(box) });
 
     // enable it, take another screenshot
     var cb = await page.locator('#application_jwtConfiguration_enabled');
@@ -59,35 +72,29 @@ test('core-concepts-applications-edit-jwt', async ({ page }) => {
 
     var css = ' #application_jwtConfiguration_enabled-form-row '+ outlineCSS;
 
-    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-jwt-enabled-configuration.png', style: css});
+    const usagePolicy = await page.getByText('Usage policy', { exact: true });
+    await expect(usagePolicy).toBeVisible();
+    const boxEnabled = await page.locator('main.page-body').boundingBox();
+
+    // TODO not sure why this doesn't get the whole page.
+    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-jwt-enabled-configuration.png', style: css, clip: calculateClip(boxEnabled) });
   
   
 });
 
-test('core-concepts-applications-edit-email', async ({ playwright}) => {
-  const browser = await playwright.webkit.launch();  
-  // Create a new browser context.
-  // want a huge viewport otherwise our nav gets all screwy (can't use fullPage: true)
-  const context = await browser.newContext({viewport: {height: 7000, width: 1280}});
-  // Create a new page in a pristine context.
-  const page2 = await context.newPage();
-    // need to wait until page loads
-  await page2.goto(baseAdminURL+'/application/edit?applicationId=85a03867-dccf-4882-adde-1a79aeec50df&tenantId=30663132-6464-6665-3032-326466613934#email-settings', {
+test('core-concepts-applications-edit-email', async ({ page}) => {
+  await page.goto(baseAdminURL+'/application/edit?applicationId=85a03867-dccf-4882-adde-1a79aeec50df&tenantId=30663132-6464-6665-3032-326466613934#email-settings', {
     waitUntil: "networkidle"
   });
 
-    // clip region from tabs down to email verification settings
-    const box = await page2.locator('main.page-body').boundingBox();
-    const bottomY = (box?.y + box?.height )|| 1200;
+  // clip region from tabs down to email verification settings
+  const box = await page.locator('main.page-body').boundingBox();
 
-
-  await page2.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-email.png',
- clip:  {height: bottomY, width: 1280, x: 0, y: 0 }
+  await page.screenshot({ 
+    animations: 'disabled', 
+    path: baseImagePath+'get-started/core-concepts/application-email.png',
+    clip: calculateClip(box)
  });
-  
-  // Gracefully close up everything
-  await context.close();
-  await browser.close();
 
 });
 
@@ -110,7 +117,9 @@ enabling these causes the navigation to be messed up by the fullPage setting
     await expect(cb).toBeChecked();
 */
 
-    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-oauth.png', fullPage: true });
+    const box = await page.locator('main.page-body').boundingBox();
+    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/application-oauth.png',
+    clip: calculateClip(box) });
   
 });
 
@@ -162,6 +171,7 @@ error
     // addRoleButton.click();
     // addRoleButton.click();
 
-  
-    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/create-application.png', fullPage: true });
+    const box = await page.locator('main.page-body').boundingBox();
+    await page.screenshot({ animations: 'disabled', path: baseImagePath+'get-started/core-concepts/create-application.png',
+    clip: calculateClip(box) });
 });
